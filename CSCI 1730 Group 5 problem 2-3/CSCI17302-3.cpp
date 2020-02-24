9806e68443f979683359e7e4c3047f4bf45530a9
snowschooled <qv9182ia@go.minneapolis.edu>
2020-02-24 15:24:58 -0600

    ready to print and subnit

--- CSCI 1730 Group 5 problem 2-3/CSCI17302-3.cpp
+++ CSCI 1730 Group 5 problem 2-3/CSCI17302-3.cpp
@@ -1,19 +1,6 @@
-//CSCI Group 5 assignment 2 problem 2-3
-//Joel Piontek, Andrew, Adem
-/*
-   3. The Fibonacci sequence is 0, 1, 1, 2, 3, 5, 8, 13, 21, …
-   where the first two terms are 0 and 1,
-   and each term thereafter is the sum of the two preceding terms.
-   Write a C++ program that repeatedly prompts for and reads a positive value n,
-   calls a recursive function fibonnaci to calculate the nth  number in the Fibonacci sequence,
-   and then displays the number. For example, if n = 9, then the program would display 21.
-   Here is the output from a sample run of the program (user input in bold):
-   Fibonnaci number generator Which one do you want (negative to exit)? 4
-   The 4th Fibbonaci number is 2 Which one do you want (negative to exit)? 5
-   The 5th Fibbonaci number is 3 Which one do you want (negative to exit)? 6
-   The 6th Fibbonaci number is 5 Which one do you want (negative to exit)? 7
-   The 7th Fibbonaci number is 8 Which one do you want (negative to exit)? -1
- */
+//CSCI Group 5 assignment 2 problem #3
+//Joel Piontek, Andrew Amakye Ansah, Adem Boru
+
 #include <iostream>
 #include <string>
 int fibonacci(int n);
@@ -31,11 +18,11 @@ int main()
 		if (n <= 0)
 			exit (1);
 		else if (n == 1)
-			cout << "The first Fibonacci number is 0 ";
+			cout << "The 1st Fibonacci number is 0 ";
 		else if (n == 2)
-			cout << "The second Fibonacci number is 1 ";
+			cout << "The 2nd Fibonacci number is 1 ";
 		else if (n==3)
-			cout<<"The third Fibonacci number is 1 ";
+			cout<<"The 3rd Fibonacci number is 1 ";
 		else if (n > 2)
 		{fibNumber = fibonacci(n);
 		 cout << "The " << n <<"th " << "Fibonacci number is: "<< fibNumber <<" ";}
@@ -46,12 +33,11 @@ int main()
 
 int fibonacci(int n)
 {
-	if((n==1)||(n==0))
-	{
-		return(n);
-	}
-	else
-	{
-		return(fibonacci(n-1)+fibonacci(n-2));
-	}
+	/*if((n==1)||(n==0))
+	   {
+	        return(n);
+	   }
+	   else
+	   {*/
+	return(fibonacci(n-1)+fibonacci(n-2));
 }
--- main#6.cpp
+++ main#6.cpp
@@ -9,65 +9,65 @@ int floor; // the floor the elevator is currently present
 
 // default constructor: set the floor to 1
 Elevator() {
-floor = 1;
+	floor = 1;
 }
 
 // set the floor
 void setFloor(int pos) {
-floor = pos;
+	floor = pos;
 }
 
 // get the floor
 int getFloor() {
-return floor;
+	return floor;
 }
 };
 
 // print the Elevators status
 void printElevatorStatus(Elevator a, Elevator b, Elevator c) {
-cout << "\nElevator Status: " << endl;
-cout << "A\tB\tC\t\n";
-cout << a.getFloor() << "\t" << b.getFloor() << "\t" << c.getFloor() << endl;
+	cout << "\nElevator Status: " << endl;
+	cout << "A\tB\tC\t\n";
+	cout << a.getFloor() << "\t" << b.getFloor() << "\t" << c.getFloor() << endl;
 }
 int main() {
-Elevator e[3]; // create an array of 3 Elevators
-int liftNumber; // the lift number of the elevator requested by user
-while(true) {
+	Elevator e[3]; // create an array of 3 Elevators
+	int liftNumber; // the lift number of the elevator requested by user
+	while(true) {
 // print the elevator status first
-printElevatorStatus(e[0], e[1], e[2]);
+		printElevatorStatus(e[0], e[1], e[2]);
 
 // ask the user his choice of elevator
-cout << "Which elevator do you want(1=A, 2=B, 3=C, or other to exit)? ";
-cin >> liftNumber;
+		cout << "Which elevator do you want(1=A, 2=B, 3=C, or other to exit)? ";
+		cin >> liftNumber;
 
 // exit for any number other than 1, 2 or 3
-if(liftNumber < 1 || liftNumber > 3) {
-return 0;
-}
+		if(liftNumber < 1 || liftNumber > 3) {
+			return 0;
+		}
 
 // If the elevator requested is not on 1st floor, move it to 1st floor
 // first
-cout << "Starting at floor " << e[liftNumber - 1].getFloor() << endl;
-for(int i = e[liftNumber - 1].getFloor() - 1; i>=1; i--) {
-cout << "\tGoing down - now at floor " << i << endl;
-}
+		cout << "Starting at floor " << e[liftNumber - 1].getFloor() << endl;
+		for(int i = e[liftNumber - 1].getFloor() - 1; i>=1; i--) {
+			cout << "\tGoing down - now at floor " << i << endl;
+		}
 //cout << "Stopping at floor 1" << endl;
- //set the elevator floor value to 1
-e[liftNumber - 1].setFloor(1);
+		//set the elevator floor value to 1
+		e[liftNumber - 1].setFloor(1);
 
 // ask the user the floor number
-int floorNumber;
-cout << "Which floor do you want? ";
-cin >> floorNumber;
+		int floorNumber;
+		cout << "Which floor do you want? ";
+		cin >> floorNumber;
 
 // Move the lift to the requested floor
-for(int i=2; i<= floorNumber; i++) {
-cout << "\tGoing up - now at floor " << i << endl;
-}
-cout << "Stopping at floor " << floorNumber << endl;
+		for(int i=2; i<= floorNumber; i++) {
+			cout << "\tGoing up - now at floor " << i << endl;
+		}
+		cout << "Stopping at floor " << floorNumber << endl;
 // set the elevator's floor value to floorNumber requested by user
-e[liftNumber - 1].setFloor(floorNumber);
-}
+		e[liftNumber - 1].setFloor(floorNumber);
+	}
 
-return 0;
+	return 0;
 }
